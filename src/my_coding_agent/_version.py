6f139324dd86e4c2f49f260235c7313fea0cd4f0
Randy Herritt <randy.herritt@gmail.com>
2025-06-08 13:44:59 -0300

    chore: update VS Code settings and version file - Update VS Code codeActionsOnSave to use explicit format - Version bump to 0.1.dev47+g36d6ea2.d20250608 (auto-generated)

--- src/my_coding_agent/_version.py
+++ src/my_coding_agent/_version.py
@@ -5,7 +5,8 @@ __all__ = ["__version__", "__version_tuple__", "version", "version_tuple"]
 
 TYPE_CHECKING = False
 if TYPE_CHECKING:
-    from typing import Tuple, Union
+    from typing import Tuple
+    from typing import Union
 
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
@@ -16,5 +17,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.dev31+g319d3ae.d20250607"
-__version_tuple__ = version_tuple = (0, 1, "dev31", "g319d3ae.d20250607")
+__version__ = version = '0.1.dev47+g36d6ea2.d20250608'
+__version_tuple__ = version_tuple = (0, 1, 'dev47', 'g36d6ea2.d20250608')
